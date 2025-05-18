5bc042ddbafc30e27a3f76dd4eed5a6f146286a0
Carl-Analytix <carl.jordan.marlow@gmail.com>
2025-05-18 12:05:58 -0500

    updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 from alerts import get_active_alerts
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +17,5 @@ def alerts():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
